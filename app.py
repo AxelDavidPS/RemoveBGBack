dc95ecbfea449d7375e798fc2121d447a3ae007e
Axel Silva <pablitobanana1@gmail.com>
2023-10-11 19:32:58 -0600

    Fist commit * Se inicializa el proyecto con su configuracion para AWS

---
+++ app.py
@@ -0,0 +1,24 @@
+import awsgi
+from flask import Flask, jsonify, make_response
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_from_root():
+    return jsonify(message='Hello from root!')
+
+
+@app.route("/hello")
+def hello():
+    return jsonify(message='Hello from path!')
+
+
+@app.errorhandler(404)
+def resource_not_found(e):
+    return make_response(jsonify(error='Not found!'), 404)
+
+
+def handler(event, context):
+    return awsgi.response(app, event, context, base64_content_types={"image/png"})
+
